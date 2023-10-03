915549df396925f808011d416953d419b3df37b3
geo <tp53.geo@gmail.com>
2023-10-03 03:28:34 +0200

    adapt gossip and other loots

--- src/app/shared/types/gossip-menu.type.ts
+++ src/app/shared/types/gossip-menu.type.ts
@@ -1,12 +1,14 @@
 import { TableRow } from './general';
 
 export const GOSSIP_MENU_TABLE = 'gossip_menu';
-export const GOSSIP_MENU_ID = 'MenuID';
-export const GOSSIP_MENU_ID_2 = 'TextID';
+export const GOSSIP_MENU_ID = 'entry';
+export const GOSSIP_MENU_ID_2 = 'text_id';
 export const GOSSIP_MENU_SEARCH_FIELDS = [GOSSIP_MENU_ID, GOSSIP_MENU_ID_2];
 export const GOSSIP_MENU_CUSTOM_STARTING_ID = 62_000;
 
 export class GossipMenu extends TableRow {
-  MenuID: number = 0;
-  TextID: number = 0;
+  entry: number = 0;
+  text_id: number = 0;
+  script_id: number = 0;
+  condition_id: number = 0;
 }
